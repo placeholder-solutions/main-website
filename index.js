fb25fe974938490150629b3108ebf8073b66caf2
placeholder-solutions <jackofwarrilow@gmail.com>
2024-06-17 17:10:47 +0100

    Add files via upload

--- index.js
+++
@@ -1,7 +0,0 @@
-let buttonClick = document.getElementById('workWithUsButton');
-
-function rerouteAlert() {
-    alert("You are being redirected to a form");
-}
-
-buttonClick.addEventListener('click', rerouteAlert);
